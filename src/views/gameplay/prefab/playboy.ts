a607a17a5aeee1f31e503b4d9dc14d2c6dc0f260
Hierifer <hierifer@hotmail.com>
2024-09-17 01:35:26 +0800

    feat: add spine

---
+++ src/views/gameplay/prefab/playboy.ts
@@ -0,0 +1,115 @@
+import { Assets, Sprite, Texture } from "pixi.js";
+import Matter from "matter-js";
+import { GameObject } from "@/modules/hex-engine/scene/GameObject";
+import {
+  SpineComponent,
+  Physics2DComponent,
+  Physics2DColliderComponent,
+  SpineAnimatorComponent,
+} from "@/modules/hex-engine/scene/Component";
+import { Prefab } from "@/modules/hex-engine/scene/Prefab";
+import { Spine } from "@pixi/spine-pixi";
+
+type Status = "pending" | "ready";
+
+export class Playboy extends Prefab {
+  label = "playboy";
+  components = [];
+  status: Status = "pending";
+  spine: Spine | undefined;
+  size: number;
+  prefab = "playboy";
+  waitingList: Array<() => Promise<GameObject | void>>;
+  constructor(label: string, { src, size }: { src?: string; size: number }) {
+    super(label);
+    this.size = size;
+    this.waitingList = [];
+
+    Promise.resolve()
+      .then(async () => {
+        //console.log(that);
+        return Promise.resolve().then(async () => {
+          await Assets.load([
+            {
+              alias: "spineSkeleton",
+              src: "https://raw.githubusercontent.com/pixijs/spine-v8/main/examples/assets/spineboy-pro.skel",
+            },
+            {
+              alias: "spineAtlas",
+              src: "https://raw.githubusercontent.com/pixijs/spine-v8/main/examples/assets/spineboy-pma.atlas",
+            },
+          ]);
+          this.status = "ready";
+        });
+      })
+      .then(() => {
+        this.waitingList.forEach((genTask) => {
+          genTask();
+        });
+        this.waitingList = [];
+      });
+
+    // 设置 physics
+  }
+
+  /**
+   * 异步生成对象
+   * @param param0
+   * @returns
+   */
+  generate({
+    x,
+    y,
+    size,
+    space,
+    label,
+  }: {
+    x: number;
+    y: number;
+    size?: number;
+    space?: string;
+    label?: string;
+  }) {
+    return new Promise<GameObject>((resolve) => {
+      const DEFAULT_SIZE = 24;
+      if (this.status === "ready") {
+        // Create a new Sprite from an image path.
+        this.spine = Spine.from({
+          skeleton: "spineSkeleton",
+          atlas: "spineAtlas",
+        });
+
+        this.spine.scale.set(0.25);
+        const box = Matter.Bodies.rectangle(
+          x,
+          y,
+          this.spine.width,
+          this.spine.height,
+          { inertia: Infinity }
+        );
+
+        resolve(
+          new GameObject(
+            label ?? "playboy",
+            { posX: x, posY: y },
+            { width: this.spine!.width, height: this.spine!.height },
+            "playboy"
+          ).addComponents([
+            new SpineComponent(this.spine, { x: 0, y: this.spine.height / 2 }),
+            new Physics2DComponent(box),
+            new Physics2DColliderComponent(space || "playboy", box),
+            new SpineAnimatorComponent(this.spine),
+          ])
+        );
+      } else {
+        this.waitingList.push(() =>
+          this.generate({ x, y, size, label, space }).then((res) =>
+            resolve(res)
+          )
+        );
+      }
+    });
+  }
+}
+
+export default Playboy;
